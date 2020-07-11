d024a0e24e9ec9498dbf90dfdae1c67ba2fa741f
Franco Nahuel Rupp <52586606+bshyn@users.noreply.github.com>
2020-07-11 03:15:02 -0300

    Revert "Agrega autenticación con JWT"

--- src/main/java/com/educacionit/student/api/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.educaciont.student.api.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "SecretKey123";
-    public static final long EXPIRATION_TIME = 3_600_000; // 1 hour
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/users/sign-up";
-}
